af0b19e11c3ab555a68fe09284171ca69ddd4361
Jimmy Higuchi <jimmyhiguchi@gmail.com>
2018-02-21 10:21:57 -0800

    Initial Commit

---
+++ Destini/ViewController.swift
@@ -0,0 +1,130 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by Philipp Muellauer on 01/09/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // Our strings
+    let story1 = "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: \"Need a ride, boy?\"."
+    let answer1a = "I\'ll hop in. Thanks for the help!"
+    let answer1b = "Better ask him if he\'s a murderer first."
+    
+    
+    let story2 = "He nods slowly, unphased by the question."
+    let answer2a = "At least he\'s honest. I\'ll climb in."
+    let answer2b = "Wait, I know how to change a tire."
+    
+    let story3 = "As you begin to drive, the stranger starts talking about his relationship with his mother. He gets angrier and angrier by the minute. He asks you to open the glovebox. Inside you find a bloody knife, two severed fingers, and a cassette tape of Elton John. He reaches for the glove box."
+    let answer3a = "I love Elton John! Hand him the cassette tape."
+    let answer3b = "It\'s him or me! You take the knife and stab him."
+    
+    let story4 = "What? Such a cop out! Did you know traffic accidents are the second leading cause of accidental death for most adult age groups?"
+    let story5 = "As you smash through the guardrail and careen towards the jagged rocks below you reflect on the dubious wisdom of stabbing someone while they are driving a car you are in."
+    let story6 = "You bond with the murderer while crooning verses of \"Can you feel the love tonight\". He drops you off at the next town. Before you go he asks you if you know any good places to dump bodies. You reply: \"Try the pier.\""
+    
+    
+    // UI Elements linked to the storyboard
+    @IBOutlet weak var topButton: UIButton!         // Has TAG = 1
+    @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
+    @IBOutlet weak var storyTextView: UILabel!
+    
+    @IBOutlet weak var restartButton: UIButton!
+    
+    
+    
+    // TODO Step 5: Initialise instance variables here
+    
+    var currentStage = 1
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
+        
+        topButton.setTitle(answer1a, for: .normal)
+        bottomButton.setTitle(answer1b, for: .normal)
+        storyTextView.text = story1
+        restartButton.isHidden = true
+        
+    }
+
+    @IBAction func restartStory(_ sender: Any) {
+        topButton.setTitle(answer1a, for: .normal)
+        bottomButton.setTitle(answer1b, for: .normal)
+        storyTextView.text = story1
+        restartButton.isHidden = true
+        topButton.isHidden = false
+        bottomButton.isHidden = false
+        
+    }
+    
+    // User presses one of the buttons
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        // TODO Step 4: Write an IF-Statement to update the views
+        if sender.tag == 1 {
+            switch sender.titleLabel?.text {
+            case answer1a?: setScreenValues(3)
+            case answer2a?: setScreenValues(3)
+            case answer3a?: setScreenValues(6)
+            default: print("end of story")
+            }
+        }
+        else if sender.tag == 2 {
+            switch sender.titleLabel?.text {
+            case answer1b?: setScreenValues(2)
+            case answer2b?: setScreenValues(4)
+            case answer3b?: setScreenValues(5)
+            default: print("end of story")
+            }
+        }
+        
+        
+        // TODO Step 6: Modify the IF-Statement to complete the story
+        
+        
+    }
+    
+    func setScreenValues(_ nextScreen: Int) {
+        switch nextScreen {
+        case 2:
+            topButton.setTitle(answer2a, for: .normal)
+            bottomButton.setTitle(answer2b, for: .normal)
+            storyTextView.text = story2
+        case 3:
+            topButton.setTitle(answer3a, for: .normal)
+            bottomButton.setTitle(answer3b, for: .normal)
+            storyTextView.text = story3
+        case 4:
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+            restartButton.isHidden = false
+            storyTextView.text = story4
+        case 5:
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+            restartButton.isHidden = false
+            storyTextView.text = story5
+        case 6:
+            topButton.isHidden = true
+            bottomButton.isHidden = true
+            storyTextView.text = story6
+            restartButton.isHidden = false
+        default:
+            print("end of story")
+            
+        }
+        
+        currentStage = nextScreen
+    }
+
+
+}
+
